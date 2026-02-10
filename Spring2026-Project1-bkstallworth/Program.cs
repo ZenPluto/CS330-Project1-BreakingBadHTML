b06aa49c8eb1cf6af0324d59d4ebad83a1c35d24
BStallworth <bkstallworth@crimson.ua.edu>
2026-02-10 16:00:58 -0600

    Add files via upload

---
+++ Spring2026-Project1-bkstallworth/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Serve index.html and other static files first
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// After static files
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
+
---
+++ Spring2026-Project1-bkstallworth/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿const characters = {
+    walter: "Walter White — Walter White begins as an underpaid, overqualified chemistry teacher struggling with pride, financial insecurity, and a sense of wasted potential. After being diagnosed with terminal lung cancer, he turns to manufacturing methamphetamine to secure his family’s future. Walt is extremely intelligent, methodical, and driven, but also deeply egotistical. As the series progresses, his original justification (“for my family”) erodes, revealing his hunger for power, control, and recognition. His transformation into Heisenberg represents the loss of his moral boundaries and the rise of his ruthless, manipulative nature.",
+    jesse: "Jesse Pinkman — Walter White's former student who helps Walter start his journey to becoming the drug kingpin Heisenberg.",
+    mike: "Mike Ehrmantraut — A fixer, hitman, and enforcer for Gus Fring.",
+    hank: "Hank Schrader — A DEA agent and Walter’s brother-in-law.",
+    gus: "Gus Fring — A calm, calculated drug kingpin who runs Los Pollos Hermanos."
+};
+
+function showCharacter(name) {
+    const infoBox = document.getElementById("characterInfo");
+    infoBox.style.display = "block";
+    infoBox.textContent = characters[name];
+}
